deccbd5d33a2115e358f33c634cdd737aee4e62b
FrankLuo426 <caesarfrank@hotmail.com>
2020-04-09 19:59:00 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-//create a search buttton
+//create a GIF search buttton
 function GIFsearchButtonClicked(){
     //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
     const GIF_Search_URL = "https://api.giphy.com/v1/gifs/search";
@@ -12,4 +12,55 @@ function GIFsearchButtonClicked(){
     url += "api_key=" + api_key;
     //get and parse the search item
     let item = document.querySelector("#searchGIF").value;
+    displayTerm = item;
+    //fix the search item problem
+    item = item.trim();
+    item = encodeURIComponent(item);
+    //make sure the item greater than 1
+    if (item.length < 1) {
+        return;
+    }
+    //parse in item
+    url += "&q=" + item;
+
+    //set and parse in the limit 
+    let limit = document.querySelector("#limitGIF").value;
+    url += "&limit=" + limit;
+
+    //update
+    document.querySelector("#status").innerHTML = "<b>Searching For '" + displayTerm + "'</b>";
+
 }
+
+function STICKERsearchButtonClicked(){
+        //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
+        const STICKER_Search_URL = "https://api.giphy.com/v1/stickers/search";
+        //url to store the value
+        //require api_key, q, limit, offset, rating, lang,  random_id.
+        //the first one is the GIF_URL
+        let url = STICKER_Search_URL;
+        //decleasr some required value
+        let api_key = "tH7NkuJUOcVVGQ7yoFMoUkTgppTJUEY9";
+        //parse in the api_key
+        url += "api_key=" + api_key;
+        //get and parse the search item
+        let item = document.querySelector("#searchSTICKER").value;
+        displayTerm = item;
+        //fix the search item problem
+        item = item.trim();
+        item = encodeURIComponent(item);
+        //make sure the item greater than 1
+        if (item.length < 1) {
+            return;
+        }
+        //parse in item
+        url += "&q=" + item;
+    
+        //set and parse in the limit 
+        let limit = document.querySelector("#limitSTICKER").value;
+        url += "&limit=" + limit;
+    
+        //update
+        document.querySelector("#status").innerHTML = "<b>Searching For '" + displayTerm + "'</b>";
+}
+

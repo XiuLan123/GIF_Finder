3810bed1154dbc124f13d46632091143112a734c
FrankLuo426 <caesarfrank@hotmail.com>
2020-04-10 00:15:08 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 //create a GIF search buttton
 function GIFsearchButtonClicked(){
     //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
-    const GIF_Search_URL = "https://api.giphy.com/v1/gifs/search";
+    const GIF_Search_URL = "https://api.giphy.com/v1/gifs/search?";
     //url to store the value
     //require api_key, q, limit, offset, rating, lang,  random_id.
     //the first one is the GIF_URL
@@ -36,7 +36,7 @@ function GIFsearchButtonClicked(){
 
 function STICKERsearchButtonClicked(){
         //#1 above - this URL is the Giphy Search endpoint. Here's an example of another endpoint, the Giphy "Trending" endpoint:
-        const STICKER_Search_URL = "https://api.giphy.com/v1/stickers/search";
+        const STICKER_Search_URL = "https://api.giphy.com/v1/stickers/search?";
         //url to store the value
         //require api_key, q, limit, offset, rating, lang,  random_id.
         //the first one is the GIF_URL
@@ -81,6 +81,48 @@ function getData(url){
 //Create the callback functions
 function dataLoaded(e){
     let xhr = e.target;
+
+
+    //puty texxt into an obj
+    let obj = JSON.parse(xhr.responseText);
+    if (obj == null || obj.data.length == 0) {
+        document.querySelector("#status").innerHTML = "<b>No Result</b> "
+    }
+
+    let results = obj.data;
+    let bigString = "<p><i>Here are " + results.length + " results for '" + displayTerm + "'</i></p>";
+
+    for (let i = 0; i < results.length; i++) {
+        let thisResult = results[i];
+
+        let smallURL = thisResult.images.fixed_width_small.url;
+        if (!smallURL) smallURL = "images/no-image-found.png";
+
+        let url = result.url;
+        let rating = result.rating.toUpperCase();
+        let title = result.title;
+
+        //copy url
+        function copyText(){
+            let URLtext = result.url;
+            URLtext.select();
+            URLtext.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+            alert("Copied the text: " + copyText.value);
+        }
+
+        var line = `<div class='result'><img src='${smallURL}' title= '${result.id}' />`;
+        line += `<span><a target='_blank' href='${url}'>'${title}'</a></span>`;
+        line += `<p>Rating: ${rating}</p></div>`;
+        line += `<button class="copyButton" onclick="copyText()">Click here to copy URL</button>`;
+        bigString += line;
+    }
+
+
+
+    document.querySelector("#content").innerHTML = bigString;
+
+    document.querySelector("#status").innerHTML = "<b>Success!</b>";
 }
 function dataError(e){
 
